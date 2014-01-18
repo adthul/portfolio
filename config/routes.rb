c16ebdd16d6804902c206551da5a9d3a623a02b9
Andrew Thul <thul.andy@gmail.com>
2014-01-17 16:20:41 -0800

    begin theme integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Portfolio::Application.routes.draw do
   resources :posts
 
-  get "welcome/index"
+  get 'about', to: 'welcome#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'index', to: 'welcome#index'
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
