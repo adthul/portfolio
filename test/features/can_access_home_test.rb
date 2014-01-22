79cb59dd4c800f9d0b4b8ecd8742ec64fe0e1b17
Andrew Thul <thul.andy@gmail.com>
2014-01-22 09:02:53 -0800

    pass tests after template integration

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class CanAccessHomeTest < Capybara::Rails::TestCase
   def test_homepage_has_content
     visit root_path
-    page.must_have_content "Andy Thul"
+    page.must_have_content "Andrew Thul"
   end
 end
--- test/features/welcome_page_test.rb
+++ test/features/welcome_page_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 feature "visit domain" do
 	scenario "view domain homepage in browser" do
 		visit "http://www.andrewthul.com"
-		page.text.must_include "Andy Thul"
+		page.text.must_include "Andrew Thul"
 	end
 end
