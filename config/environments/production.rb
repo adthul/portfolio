cef2570ad601932b63135981cac04a891d0e0a14
Andrew Thul <thul.andy@gmail.com>
2014-02-21 13:50:00 -0800

    Add AWS region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Portfolio::Application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
+    :region => "us-west-2",
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
