a839a9c301bc926c69006d39799bcb53012c8dc5
Andrew Thul <thul.andy@gmail.com>
2014-02-27 14:04:45 -0800

    fix stupidity

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 if Rails.env.production?
-  Paperclip::attachment.default_options[:url] = ':s3_path_url'
-  Paperclip::attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  Paperclip::Attachment.default_options[:url] = ':s3_path_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
